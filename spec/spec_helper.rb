59dce9c414884481cc00d7fd9de2cab1fc9913a7
r7kamura <r7kamura@gmail.com>
2020-11-28 03:46:24 +0900

    Add be_as_json matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,23 @@ RSpec.configure do |config|
 
   config.run_all_when_everything_filtered = true
 end
+
+require 'active_support/core_ext/hash/keys'
+require 'active_support/core_ext/object/json'
+
+RSpec::Matchers.define :be_as_json do |expected|
+  match do |actual|
+    target = actual.as_json
+    target = begin
+      case target
+      when Array
+        target.map(&:deep_symbolize_keys)
+      when Hash
+        target.deep_symbolize_keys
+      else
+        target
+      end
+    end
+    values_match? expected, target
+  end
+end
